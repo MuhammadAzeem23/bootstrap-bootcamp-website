3afbd31f4aeb0313ef907a2f9ada13d18762eea2
MuhammadAzeem23 <muhammad.azeemt001@gmail.com>
2023-04-26 01:31:50 +0500

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibXVoYW1tYWQtYXplZW0iLCJhIjoiY2xncHA0MG15MDF4ajNmbWluMHBub2Z5dSJ9.0f3L3TFxjk-tvG1mi6zQcg';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982, 42.35725],
+    zoom: 18,
+  });
